bebc8cbcc59494861189ecd17c5aa20759320a2f
InLoveWithHer <vadim.savkunov7@gmail.com>
2021-11-27 01:41:57 +0200

    db configuration

--- db.js
+++ db.js
@@ -1,12 +1,23 @@
 const {Sequelize} = require('sequelize')
 
-module.exports = new Sequelize(
-    process.env.DB_NAME,
-    process.env.DB_USER,
-    process.env.DB_PASSWORD,
-    {
+
+if (process.env.DATABASE_URL) {
+    module.exports = new Sequelize(process.env.DATABASE_URL, {
         dialect: 'postgres',
-        host: process.env.DB_HOST,
-        port: process.env.DB_PORT
-    }
-)
+        protocol: 'postgres',
+        dialectOptions: {
+            ssl: true
+        }
+    });
+} else {
+    module.exports = new Sequelize(
+        process.env.DB_NAME,
+        process.env.DB_USER,
+        process.env.DB_PASSWORD,
+        {
+            dialect: 'postgres',
+            host: process.env.DB_HOST,
+            port: process.env.DB_PORT
+        }
+    )
+}
