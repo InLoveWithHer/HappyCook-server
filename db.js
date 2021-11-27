9044a62fb55ce218477d5a29c6cb935b16f5d835
InLoveWithHer <vadim.savkunov7@gmail.com>
2021-11-27 02:15:53 +0200

    ssl true

--- db.js
+++ db.js
@@ -6,7 +6,7 @@ if (process.env.DATABASE_URL) {
         dialect: 'postgres',
         protocol: 'postgres',
         dialectOptions: {
-            ssl: false
+            ssl: true
         }
     });
 } else {
